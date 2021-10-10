c81770e2406aa13e0225f4a965b9b8caec2af887
Michael D'Arcy <mjd-programming@gmail.com>
2021-10-10 18:51:30 -0400

    Audio peeler python file does not work with test_api.html, testing.txt contains testing plan

--- audio_peeler_api.py
+++ audio_peeler_api.py
@@ -1,16 +1,19 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+    
+def send_file_to_demucs(file):
+    print(file + ' is being sent to demucs')
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route("/", methods=["POST"])
 def hello():
-    if request.method == 'GET':
-        return 'here is your data!'
     if request.method == 'POST':
         received_song = request.get_data()
-        
-        return 'received data'
-    return ''
+        components = send_file_to_demucs(received_song)
+        # song will be handled by demucs and the compoent files will be returned
+        return 'received data: ' + components + '.'
+    return 'error'
 
 if __name__ == '__main__':
     app.run(debug=True)
