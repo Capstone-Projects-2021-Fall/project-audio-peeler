e8cc0f1589fcdd24e67fc0b250ae1623e1b5b63d
joshv1499 <84vazquezj@gmail.com>
2021-10-14 11:17:39 -0400

    Added ability to select a file for upload

--- audio-peeler-web-app/src/App.js
+++ audio-peeler-web-app/src/App.js
@@ -1,33 +1,48 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <h4>
-            Welcome to
-          </h4>
-          <h2>
-            AudioPeeler!
-          </h2>
-        </div>
-        
-        <button>
-          Browse
-        </button>
-        <div>
-          music.mp3
-          (the are will play audio file later)
-        </div>
-        
-        <div class="output_area">
-          All the output will be here...
-        </div>
-      </header>
-    </div>
-  );
+  state = { selectedFile: null }
+
+  fileChangedHandler = (event) => {
+    const file = event.target.files[0];
+  }
+
+  uploadHandler = () => {
+    console.log(this.state.selectedFile)
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <h4>
+              Welcome to
+            </h4>
+            <h2>
+              AudioPeeler!
+            </h2>
+          </div>
+          <input type="file" onChange={this.fileChangedHandler} />
+          <br/>
+          
+          
+          <button onClick={this.uploadHandler}>
+            Submit
+          </button>
+          <div>
+            music.mp3
+            (the are will play audio file later)
+          </div>
+
+          <div class="output_area">
+            All the output will be here...
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
