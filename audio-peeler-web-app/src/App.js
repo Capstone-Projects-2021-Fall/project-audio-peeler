b3e31e81f292c68d299f294f49d63bf9ce7dc1eb
Yang Bo <VictorBoYang@gmail.com>
2021-10-15 15:35:18 -0400

    Revert "Initial Setup for React Native w/ Typescript"
    This reverts commit 96f5d4e6bf0ac2efa59e3b4cbc8fa05ebe994c8d.

--- audio-peeler-web-app/src/App.js
+++ audio-peeler-web-app/src/App.js
@@ -1,68 +1,66 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+class App extends Component {
 
+  state = { selectedFile: null }
 
+  fileSelectedHandler = (event) => {
+    this.setState({
+      selectedfile : event.target.files[0]
+    })
+    
+  }
 
-
-
-  function App() {
-    const [selectedFile, setSelectedFile] = useState();
-    const [isFilePicked, setIsFilePicked] = useState(false);
-
-    const changeHandler = (event) => {
-      setSelectedFile(event.target.files[0]);
-      setIsFilePicked(true);
-    };
-
-
-    function uploadHandler(){
-
-      const formData = new FormData()
-      formData.append(
-          'myFile',
-          this.state.selectedFile,
-          //this.state.selectedFile.name
-      )
-      axios.post('http://172.105.151.238:5000/test', formData, {
-        onUploadProgress: ProgressEvent => {
-          console.log(ProgressEvent.loaded / ProgressEvent.total)
-        }
-      })
-          .then(res => {
-            console.log(res)
-          })
-    }
-
+  uploadHandler = () => {
+    
+    const formData = new FormData()
+    formData.append(
+      'myFile',
+      this.state.selectedFile,
+      //this.state.selectedFile.name
+    )
+    axios.post('http://172.105.151.238:5000/test', formData, {
+      onUploadProgress: ProgressEvent => {
+        console.log(ProgressEvent.loaded / ProgressEvent.total)
+      }
+    })
+    .then(res => {
+      console.log(res)
+    })
+  }
+  render() {
     return (
-        <div className="App">
-          <header className="App-header">
-            <div>
-              <h4>
-                Welcome to
-              </h4>
-              <h2>
-                AudioPeeler!
-              </h2>
-            </div>
-
-            <input title={"sup"} type="file" onChange={changeHandler} className={"file_button"}/>
-            <br/>
-
-            {isFilePicked ? (<p className={"text"}> {selectedFile.name} </p>) : (<p className={"text"}> Nothing to see here </p>)}
-
-            <button className={"button"} onClick={uploadHandler}>
-              Submit
-            </button>
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <h4>
+              Welcome to
+            </h4>
+            <h2>
+              AudioPeeler!
+            </h2>
+          </div>
+          <input type="file" onChange={this.fileSelectedHandler} />
+          <br />
 
 
-            <div class="output_area">
-              All the output will be here...
-            </div>
-          </header>
-        </div>
+          <button onClick={this.uploadHandler}>
+            Submit
+          </button>
+          <div>
+            music.mp3
+            (the are will play audio file later)
+          </div>
+
+          <div class="output_area">
+            All the output will be here...
+          </div>
+        </header>
+      </div>
     );
-
+  }
 }
 
 export default App;
