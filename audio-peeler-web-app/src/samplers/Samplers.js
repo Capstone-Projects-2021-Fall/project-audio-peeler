9b2cb9268338ffe09948deff93967360e9a31978
Michael D'Arcy <mjd-programming@gmail.com>
2021-10-29 16:42:07 -0400

    milestone 2 commit

--- audio-peeler-web-app/src/samplers/Samplers.js
+++ audio-peeler-web-app/src/samplers/Samplers.js
@@ -1,28 +1,53 @@
 import axios from "axios";
 import './Samplers.css';
 import React, { useState } from 'react';
+import Icon from '@mdi/react'
+import {mdiPlay} from '@mdi/js'
+import {mdiDownload} from '@mdi/js'
 import Home from "../home/Home";
 
 function Samplers(){
 
+    var getSampleComponent = function(name) {
+        return (
+            <div class="sample-component">
+                {name}
+                <br/>
+                <div class="play-component"><Icon className="icon" path={mdiPlay} size={1.5}/></div>
+                <div class="download-component"><Icon className="icon" path={mdiDownload} size={1.5}/></div>
+            </div>
+        )
+    }
+
+    var getSampleObject = function(name) {
+        return (
+            <div class="sample">
+                <div class="sample-title">{name}</div>
+                <div class="components">
+                    {getSampleComponent('Drums')}
+                    {getSampleComponent('Bass')}
+                    {getSampleComponent('Vocals')}
+                    {getSampleComponent('Other')}
+                </div>
+            </div>
+        );
+    }
+
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <div>
-                    <h4>
-                        Lets
-                    </h4>
-                    <h2>
-                        Mash it up!
-                    </h2>
+        <div id="page">
+            <div id="main-content-samplers">
+                <div id="title-area">
+                    <h1>Samplers</h1>
+                    <h3>Here are a few picks from the team</h3>
                 </div>
-
-
-                <div class="output_area">
-                    Welcome to the mashup output area!
+                <div id="samples">
+                    {getSampleObject("Snake Eater")}
+                    {getSampleObject("Que Sera Sera")}
+                    {getSampleObject("Through the Wire")}
+                    {getSampleObject("Fly Me to the Moon")}
                 </div>
-            </header>
+            </div>
         </div>
     )};
 
