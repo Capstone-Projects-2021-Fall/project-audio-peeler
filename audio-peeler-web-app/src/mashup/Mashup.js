187885651513e76f7c82afc7d9749f87efb2d052
KevlarBoxers <chessegrater@gmail.com>
2021-11-01 13:46:03 -0400

    Fixing merge conflicts

--- audio-peeler-web-app/src/mashup/Mashup.js
+++ audio-peeler-web-app/src/mashup/Mashup.js
@@ -1,28 +1,46 @@
-import axios from "axios";
 import './Mashup.css';
-import React, { useState } from 'react';
-import Home from "../home/Home";
+import React from 'react';
 
 function Mashup(){
 
+    var changeHandler = function() {
+
+    }
+
+    var uploadHandler = function() {
+
+    }
+
+    var isFilePicked = false;
+    var selectedFile = null;
+
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <div>
-                    <h4>
-                        Lets
-                    </h4>
-                    <h2>
-                        Mash it up!
-                    </h2>
+        <div id="page">
+            <div id="main-content">
+                <div id="title-area">
+                    <h1>Mash it up!</h1>
+                    <br/>
+                    <h3>Pick Two Songs</h3>
+                    <h3>And We'll Give You Every Variation between the Two</h3>
                 </div>
-
-
-                <div class="output_area">
-                    Welcome to the mashup output area!
+                <div id="mashup-files-container">
+                    <div class="mashup-file">
+                        <input type="file" onChange={changeHandler} id="browse-button"/>
+                        {isFilePicked ? (<p className={"file-name"}> {selectedFile.name} </p>) : (<p id={"file-name"}>No file selected...</p>)}
+                    </div>
+                    <div class="mashup-file">
+                        <input type="file" onChange={changeHandler} id="browse-button"/>
+                        {isFilePicked ? (<p className={"file-name"}> {selectedFile.name} </p>) : (<p id={"file-name"}>No file selected...</p>)}
+                    </div>
                 </div>
-            </header>
+                <button id="start-button" onClick={uploadHandler}>Start</button>
+            </div>
+            <div id="output-area">
+                Output
+                <div id="output-file-name">No download ready...</div>
+                <button id="download">Download</button>
+            </div>
         </div>
     )};
 
--- audio-peeler-web-app/src/samplers/Samplers.js
+++ audio-peeler-web-app/src/samplers/Samplers.js
@@ -1,28 +1,113 @@
-import axios from "axios";
 import './Samplers.css';
-import React, { useState } from 'react';
-import Home from "../home/Home";
+import React from 'react';
 
-function Samplers(){
+function Samplers() {
 
+    var audios = [];
+    var urls = [    //snake eater
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/Snake_Eater_Short/drums.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/Snake_Eater_Short/bass.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/Snake_Eater_Short/vocals.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/Snake_Eater_Short/other.wav",
+        // dum surfer
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/King+Krule+-+Dum+Surfer/drums.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/King+Krule+-+Dum+Surfer/bass.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/King+Krule+-+Dum+Surfer/vocals.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/King+Krule+-+Dum+Surfer/other.wav",
+        //psycho Killer
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/PsychoKiller/drums.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/PsychoKiller/drums.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/PsychoKiller/vocals.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/PsychoKiller/other.wav",
+        // Fly me to the moon
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/Fly/drums.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/Fly/bass.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/Fly/vocals.wav",
+        "https://audiopeelerfilestorage.s3.us-east-2.amazonaws.com/demucs_quantized/Fly/other.wav"
+    ];
+    var pp = {};
+
+    var globalCurrentID = 0;
+
+    for (var i = 0; i < urls.length; i++) {
+        var a = new Audio(urls[i]);
+        let element = i;
+        pp[element] = false;
+        a.addEventListener('play', (event) => {
+            pp[element] = true;
+        });
+        a.addEventListener('pause', (event) => {
+            pp[element] = false
+        });
+        audios.push(a);
+    }
+
+    function play(index) {
+        var elementIndex = parseInt(index);
+        audios[elementIndex].play();
+        if (pp[elementIndex]) {
+            audios[elementIndex].pause();
+        }
+    }
+
+    function download(index) {
+        var url = urls[index];
+        var filename = url.substring(url.lastIndexOf("/") + 1).split("?")[0];
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = function() {
+            var a = document.createElement('a');
+            a.href = window.URL.createObjectURL(xhr.response);
+            a.download = filename;
+            a.style.display = 'none';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        };
+        xhr.open('GET', url);
+        xhr.send();
+    }
+
+    var getSampleComponent = function(name, i) {
+        return (
+            <div className="sample-component">
+                {name}
+                <br/>
+                <div className="play-component" onClick={() => play(i)}></div>
+                <div className="download-component" onClick={() => download(i)}></div>
+            </div>
+        )
+    }
+
+    var getSampleObject = function(name, cid) {
+        var currentID = cid;
+        return (
+            <div className="sample">
+                <div className="sample-title">{name}</div>
+                <div className="components">
+                    {getSampleComponent('Drums', currentID++)}
+                    {getSampleComponent('Bass', currentID++)}
+                    {getSampleComponent('Vocals', currentID++)}
+                    {getSampleComponent('Other', currentID++)}
+                </div>
+            </div>
+        );
+    }
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <div>
-                    <h4>
-                        Lets
-                    </h4>
-                    <h2>
-                        Mash it up!
-                    </h2>
+        <div id="page">
+            <div id="main-content-samplers">
+                <div id="title-area">
+                    <h1>Samplers</h1>
+                    <h3>Here are a few picks from the team</h3>
                 </div>
-
-
-                <div class="output_area">
-                    Welcome to the mashup output area!
+                <div id="samples">
+                    {getSampleObject("Snake Eater", globalCurrentID)}
+                    {getSampleObject("Dum Surfer", globalCurrentID+=4)}
+                    {getSampleObject("Psycho Killer", globalCurrentID+=4)}
+                    {getSampleObject("Fly Me to the Moon", globalCurrentID+=4)}
                 </div>
-            </header>
+            </div>
         </div>
     )};
 
