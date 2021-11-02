b4fc205edb899ff75b520680653b9349639ad3f5
KevlarBoxers <chessegrater@gmail.com>
2021-11-02 15:52:00 -0400

    Added helper functions in Select to select simple items within the webapp that should work on every page.
    Added helper functions in a renamed class SingleSongSeparatorFileUpload (used to be Demucs) to target specific features relating to the file upload seperation feature.
    
    Added new tests in Runnable to utilize the new functions.

--- selenium/src/main/java/utilities/Select.java
+++ selenium/src/main/java/utilities/Select.java
@@ -5,27 +5,95 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class Select {
 
     public static ChromeDriver driver;
+    String url = "http://localhost:3000";
     public static WebDriverWait wait;
 
-
-    public Select(ChromeDriver driver, WebDriverWait wait) throws IOException {
-        Select.driver = driver;
-        Select.wait = wait;
+    public Select(ChromeDriver driver, String...url) throws IOException {
+        this.url = (url.length == 0) ? localSelector() : url[0];
+        this.driver = driver;
     }
 
-    public void getURL(String url) {
+    public String localSelector() throws IOException {
+        int responseCode = getResponseCode("http://localhost:3000");
+
+        if(responseCode == 200){
+            return "http://localhost:3000";
+        }
+
+        responseCode = getResponseCode("http://localhost:3001");
+
+        if(responseCode == 200){
+            return "http://localhost:3001";
+        }
+
+        return "";
+
+    }
+
+    /**
+     * A url is submitted and it returns the response code, the intention is to detect the environment currently being used during testing. It checks localhost availability
+     * if no url was initially passed for the argvar.
+     *
+     * @param urlInput
+     * @return
+     * @throws IOException
+     */
+    private int getResponseCode(String urlInput) throws IOException {
+        URL url = new URL(urlInput);
+        HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+
+        int responseCode = huc.getResponseCode();
+        return responseCode;
+    }
+
+    /**
+     * Loads the url set in the constructor.
+     */
+    public void getURL() {
         driver.get(url);
     }
 
+    /**
+     * Returns the number of buttons on the page by tagName("button")
+     *
+     * @return int Button size
+     */
     public int getButtonsCount(){
-        List<WebElement> buttons = driver.findElements(By.tagName("button"));
+        List<WebElement> buttons = getButtons();
         return buttons.size();
+    }
+
+    /**
+     * Returns all web elements with the tagname("button"), will be useful for interfacing with the webapp.
+     *
+     * @return List<WebElement> Button
+     */
+    public List<WebElement> getButtons() {
+        return driver.findElements(By.tagName("button"));
+    }
+
+    /**
+     * Nav buttons are actually divs when rendered on the webapp and therefore requires a different method of accessing them for testing.
+     *
+     * @return List<WebElement> NavButtons
+     */
+    public int getNavButtonsCount(){
+        List<WebElement> buttons = driver.findElements(By.className("nav-button"));
+        return buttons.size();
+    }
+
+    public boolean fileExists(String fileLocation){
+        File file = new File(fileLocation);
+        return file.exists();
     }
 
 }
--- selenium/src/test/java/Runnable.java
+++ selenium/src/test/java/Runnable.java
@@ -1,4 +1,5 @@
-import cases.Demuc;
+import cases.SingleSongSeparatorFileUpload;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -6,9 +7,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.Select;
 import utilities.Setup;
 
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.List;
 
 public class Runnable {
 
@@ -20,15 +21,37 @@ public class Runnable {
     public static void init() throws IOException {
         setup = new Setup();
         wait = new WebDriverWait(setup.driver, Duration.ofSeconds(2));
-        utils = new Select(setup.driver, wait);
+        utils = new Select(setup.driver, "https://audiopeeler.herokuapp.com/");
     }
 
     @Test
     public void mainPageButtonCountCheck() throws IOException {
-        Demuc shuffle = new Demuc(setup.driver, wait);
-
+        utils.getURL();
+        int buttonCount = utils.getNavButtonsCount() + utils.getButtonsCount();
         //Input file button is not considered a button, therefore it should be one less than the expected number
-        Assert.assertEquals(1, utils.getButtonsCount());
+        Assert.assertEquals(7, buttonCount);
+    }
+
+    @Test
+    public void getSongDuration() throws IOException, UnsupportedAudioFileException, InterruptedException {
+        utils.getURL();
+        SingleSongSeparatorFileUpload shuffle = new SingleSongSeparatorFileUpload(setup.driver, "C:\\Users\\frank\\Desktop\\Que Sera Sera 30secs.mp3", "D:\\Downloads\\", "Que Sera Sera 30secs");
+        long duration = shuffle.getSongDuration();
+        Assert.assertEquals(30877, duration);
+        shuffle.downloadZip();
+        System.out.println(duration);
+    }
+
+    @Test
+    public void verifyDownload() throws UnsupportedAudioFileException, IOException, InterruptedException {
+        utils.getURL();
+        SingleSongSeparatorFileUpload shuffle = new SingleSongSeparatorFileUpload(setup.driver,"C:\\Users\\frank\\Desktop\\Que Sera Sera 30secs.mp3","D:\\Downloads\\","Que_Sera_Sera_30secs");
+        Assert.assertEquals(true,shuffle.downloadZip());
+    }
+
+    @AfterClass
+    public static void cleanUp() {
+        Setup.driver.close();
     }
 
 }
